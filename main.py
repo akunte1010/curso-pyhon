c05ac84237a6c7ec5ec4407ad02a6a431340b414
akunte1010 <akumtakumt@gmail.com>
2025-09-16 10:52:45 -0600

    Codigo clases

---
+++ main.py
@@ -0,0 +1,31 @@
+from dataclasses import dataclass
+
+@dataclass()
+class Numero:
+    numero: int
+    def __init__(self, numero):
+        self.numero = numero
+
+    def evaluarnumero(self):
+        if self.numero & 1:
+            print("impar")
+            if self.numero == 7:
+                print("\n El numero ingresado es un comodin")
+
+        else:
+            print("par")
+            if self.numero == 10:
+                print("\n El numero ingresado es 10")
+
+
+    def sumar(self ,numeroasumar):
+        return self.numero + numeroasumar
+
+
+if __name__ == "__main__":
+    numeroaevaluar = Numero(int(input("Ingrese un numero:\n")))
+    numeroaevaluar.evaluarnumero()
+    sumarealizada = numeroaevaluar.sumar(2)
+    print("\n la suma realizada es: ",sumarealizada)
+
+    
